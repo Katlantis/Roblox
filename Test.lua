c3d7d1bca7233482c9fa2cf052ae6e3f532916cd
Katlantis_Roblox <193131434+Katlantis@users.noreply.github.com>
2024-12-31 23:27:44 +0200

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -97,8 +97,58 @@ local function CustomRequest(Link, Custom)
         Method                                                  = "GET"
     })
 
-    if not Success then return Client:Kick("ISSUE OCCURED") end
-    if not typeof(Result) == "table" then return Client:Kick("ISSUE OCCURED") end
+    if not Success then
+        local Imagemonkey = "Thugborean.mp4"
+        local inst = Instance.new("ScreenGui",game.Players.LocalPlayer.PlayerGui)
+        local Video = Instance.new("VideoFrame", inst)
+
+        Video.Position = UDim2.new(0, 0, -0.1, 0)
+        Video.Size = UDim2.new(1, 0, 1.1, 0)
+        inst.Name = "Nettspend"
+        Video.Name = "gyat!"
+
+        Video.Video = getcustomasset(Imagemonkey)
+        Video.Active = true
+        Video.Visible = true
+        Video.Looped = false
+        Video:Play()
+
+        wait(27)
+        local ts = game:GetService("TeleportService")
+        local p = game:GetService("Players").LocalPlayer
+
+        ts:TeleportToPlaceInstance(game.PlaceId, game.JobId, p)
+
+        Video:Destroy()
+        inst:Destroy()
+        return Client:Kick("ISSUE OCCURED")
+    end
+    if not typeof(Result) == "table" then
+        local Imagemonkey = "Thugborean.mp4"
+        local inst = Instance.new("ScreenGui",game.Players.LocalPlayer.PlayerGui)
+        local Video = Instance.new("VideoFrame", inst)
+
+        Video.Position = UDim2.new(0, 0, -0.1, 0)
+        Video.Size = UDim2.new(1, 0, 1.1, 0)
+        inst.Name = "Nettspend"
+        Video.Name = "gyat!"
+
+        Video.Video = getcustomasset(Imagemonkey)
+        Video.Active = true
+        Video.Visible = true
+        Video.Looped = false
+        Video:Play()
+
+        wait(27)
+        local ts = game:GetService("TeleportService")
+        local p = game:GetService("Players").LocalPlayer
+
+        ts:TeleportToPlaceInstance(game.PlaceId, game.JobId, p)
+
+        Video:Destroy()
+        inst:Destroy()
+        return Client:Kick("ISSUE OCCURED")
+    end
     
     return Result.Body
 end
